eac639f86eb5ed1c3711a34b653fcd18dc7d4329
Ayush kumar <ayush.17862@stu.upes.ac.in>
2025-06-25 19:14:40 +0530

    Fix formatting and enhance comments in coin toss game

--- coin_toss.py
+++ coin_toss.py
@@ -10,3 +10,7 @@ while True:
         break
     else:
         print("Invalid input. Please enter 'yes' or 'no'.")
+
+# This code simulates a coin toss game where the user can choose to toss the coin or exit the game.
+# It uses the random module to randomly select between 'Heads' and 'Tails'. # The game continues until the user decides to stop by entering 'no'.
+# The input is validated to ensure the user enters either 'yes' or 'no'.    
